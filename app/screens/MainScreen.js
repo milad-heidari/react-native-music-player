0a6e7f0f12e310c29243aea274223a52dc43f7fe
milad heidari <milad.heidari.dev@gmail.com>
2021-09-12 00:32:54 +0430

    move useTrackPlayerEvents to up in MainScreen.js

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -5,43 +5,46 @@ import TrackPlayer,{Capability,Event,useTrackPlayerEvents} from 'react-native-tr
 import tracksActions from '../store/TracksRedux/tracksActions';
 
 
-function MainScreen({setCurrentTrackToRedux,tracks}) {
+function MainScreen({ setCurrentTrackToRedux, tracks }) {
+
+    useTrackPlayerEvents([Event.PlaybackTrackChanged], async (event) => {
+        if (event.type === Event.PlaybackTrackChanged && event.nextTrack != null) {
+            // isTrackChanged = true;
+            // const trackID = await TrackPlayer.getCurrentTrack();
+            console.log('useTrackPlayerEvents next', event.nextTrack)
+            console.log('useTrackPlayerEvents prev', event.track)
+            const track = await TrackPlayer.getTrack(event.nextTrack);
+            // const getTrackInformationObject = await TrackPlayer.getTrack(trackID);
+            await setCurrentTrackToRedux(track);
+            // const image =`data:image/jpg;base64,${currentTrackFromRedux.artwork}`
+        }
+    });
 
     let isTrackChanged = false;
     useEffect(async() => {
-        console.log(' mainScreen Inside useEffect')
-      isTrackChanged = false;
+    isTrackChanged = false;
+    const createTR = async () => {
+        // console.log(' mainScreen Inside useEffect')
      await TrackPlayer.setupPlayer()
-    //  await TrackPlayer.updateOptions({
-    //     stopWithApp: true,
-    //     capabilities: [
-    //       Capability.Play,
-    //       Capability.Pause,
-    //       Capability.JumpForward,
-    //       Capability.JumpBackward,
-    //       Capability.SkipToNext,
-    //       Capability.SkipToPrevious,
-    //     ],
-    //     compactCapabilities: [Capability.Play, Capability.Pause],
-    //   });
-      /**
-       * in below line code add music tracks to the player:
-       *   tracks: this array get from tracksRedux and set for player.
-       */
-     await TrackPlayer.add(tracks);
-      useTrackPlayerEvents ([Event.PlaybackTrackChanged], async (event) => {
-        if (event.type === Event.PlaybackTrackChanged && event.nextTrack != null) {
-            // const track = await TrackPlayer.getTrack(event.nextTrack);
-            // const {title} = track || {};
-            // setTrackTitle(title);
-            isTrackChanged = true;
-            const trackID = await TrackPlayer.getCurrentTrack();
-            console.log('tttdfdftttt', TrackPlayer.getCurrentTrack())
-            const getTrackInformationObject = await TrackPlayer.getTrack(trackID);
-            await setCurrentTrackToRedux(getTrackInformationObject);
-            // const image =`data:image/jpg;base64,${currentTrackFromRedux.artwork}`
-        }
-      });
+        await TrackPlayer.updateOptions({
+            stopWithApp: true,
+            capabilities: [
+                Capability.Play,
+                Capability.Pause,
+                Capability.JumpForward,
+                Capability.JumpBackward,
+                Capability.SkipToNext,
+                Capability.SkipToPrevious,
+            ],
+            compactCapabilities: [Capability.Play, Capability.Pause],
+        });
+        /**
+         * in below line code add music tracks to the player:
+         *   tracks: this array get from tracksRedux and set for player.
+         */
+        await TrackPlayer.add(tracks);
+    }
+    createTR()
     }, [isTrackChanged]);
 
 
