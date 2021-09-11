2f9793c595a08bb070241b1da9def2292b271eb8
milad heidari <milad.heidari.dev@gmail.com>
2021-09-12 00:35:01 +0430

    create reset dispatch for TracksRedux

--- app/store/TracksRedux/tracksReducer.js
+++ app/store/TracksRedux/tracksReducer.js
@@ -1,47 +1,57 @@
 import tracksActions from './tracksActions'
 const initState = {
-    tracks:[],
-    sortedTracks:[],
-    sortedFilterItem:[],
-    currentTrack:{},
-    currentArtwork:null
+    tracks: [],
+    sortedTracks: [],
+    sortedFilterItem: [],
+    currentTrack: {},
+    currentArtwork: null
 }
 
-const tracksReducer = (state = initState , action)=>{
+const tracksReducer = (state = initState, action) => {
 
     let newState = state
 
-    switch (action.type){
+    switch (action.type) {
         case tracksActions.ADD_MUSIC_TRACKS:
-            console.log('REDUSER ADD_MUSIC_TRACKS',action.payload.title)
-            newState = {...state,tracks:[...state.tracks,action.payload]}
+            console.log('REDUSER ADD_MUSIC_TRACKS', action.payload.title)
+            newState = { ...state, tracks: [...state.tracks, action.payload] }
             break;
-            case tracksActions.SET_CURRENT_TRACK:
-            console.log('payloadsdffffff',action.payload.title)
-            
-            newState = {...state,currentTrack:action.payload}
+        case tracksActions.SET_CURRENT_TRACK:
+            console.log('payloadsdffffff', action.payload.id)
+
+            newState = { ...state, currentTrack: action.payload }
             break;
         case tracksActions.SET_CURRENT_ARTWORK:
-            newState = {...state,currentArtwork:action.payload}
+            newState = { ...state, currentArtwork: action.payload }
             break;
         case tracksActions.ADD_SORTED_TRACKS:
-             console.log('REDUSER UPDATE',action.payload.title)
-            newState = {...state,sortedTracks:[...state.sortedTracks,action.payload]}
+            console.log('REDUSER UPDATE', action.payload.title)
+            newState = { ...state, sortedTracks: [...state.sortedTracks, action.payload] }
             // [...state.sortedTracks,action.payload]
             break;
         case tracksActions.UPDATE_SORTED_TRACKS:
             // console.log('REDUSER UPDATE',action.payload)
-            newState = {...state,sortedTracks:action.payload}
+            newState = { ...state, sortedTracks: action.payload }
             break;
         case tracksActions.ADD_SORTED_fILTER_ITEM:
-            newState = {...state,sortedFilterItem:action.payload}
+            newState = { ...state, sortedFilterItem: action.payload }
             break;
-            default:
-            break;
+        case tracksActions.RESET_TRACK_REDUX:
+            console.log('reserT')
+            newState = {
+                tracks: [],
+                sortedTracks: [],
+                sortedFilterItem: [],
+                currentTrack: {},
+                currentArtwork: null
             }
-            // console.log('payloadsdfffffsssssf',newState)
-            
+            break;
+        default:
+            break;
+    }
+    // console.log('payloadsdfffffsssssf',newState)
+
     return newState
-}  
+}
 
 export default tracksReducer;
